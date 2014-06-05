b82f7c033690ff7f6b7fada550cadb5031ea2feb
Jason Gross <t-jagro@microsoft.com>
2014-06-05 13:02:32 +0100

    Make pushd-root find a makefile

--- timing/pushd-root.sh
+++ timing/pushd-root.sh
@@ -10,3 +10,11 @@ pushd "$DIR" 1>/dev/null
 # now change to the git root; use `cd` so we only need one `popd`
 ROOT_DIR="$(git rev-parse --show-toplevel)"
 cd "$ROOT_DIR"
+
+# now find a makefile
+MAKEFILE="$(git ls-files | grep 'Makefile$\|makefile$\|GNUmakefile$' | tail -1)"
+if [ ! -z "$MAKEFILE" ]; then
+    cd "$(dirname "$MAKEFILE")"
+else
+    echo "WARNING: No Makefile,makefile,GNUmakefile found in git ls-files"
+fi
