58a1be4d092949c7301dd4b500ceab547147727c
Jason Gross <jgross@mit.edu>
2014-07-22 08:37:25 -0400

    Add a script to cat .timing files
    Only if they exist

---
+++ timing/cat-timing-files.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+# in case we're run from out of git repo
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+source "$DIR/../pushd-root.sh"
+
+# exit immediately if killed
+trap "exit 1" SIGHUP SIGINT SIGTERM
+
+if [ -z "$(fine . -name "*.timing")" ]
+then
+    exit 0 # no timing files exist
+fi
+
+for i in $(find . -name "*.timing")
+do
+    echo "$i"
+    cat "$i"
+    # echo a final new line, because `cat` doesn't
+    echo
+    # and another for whitespace
+    echo
+done
