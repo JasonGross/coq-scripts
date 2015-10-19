0d52baf6fc55629436a6cb7358065ff56dc0cc0e
Jason Gross <jgross@mit.edu>
2015-10-19 17:01:31 -0400

    Add a coqdep strictification script
    This allows making messages like
    *** Error: in file src/BinEncoders/take3/List2.v,
        required library Base matches several files in path
        (found Base.v in src/BinEncoders and src/BinEncoders/take3; used the latter)
    into errors

---
+++ strictify-coqdep.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+import sys, subprocess
+import re
+
+if __name__ == '__main__':
+    p = subprocess.Popen(sys.argv[1:], stderr=subprocess.PIPE)
+    (stdout, stderr) = p.communicate()
+    reg = re.compile(r'''Warning(: in file .*?,\s*required library .*? matches several files in path)''')
+    if reg.search(stderr):
+        sys.stderr.write(reg.sub(r'Error\1', stderr))
+        sys.exit(1)
+    sys.stderr.write(stderr)
+    sys.exit(p.returncode)
