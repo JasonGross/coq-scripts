7ec5c99691dbfd303d5844f8e9f9969144f0c746
Jason Gross <jgross@mit.edu>
2015-12-30 16:02:38 -0500

    Fix qualification problems in the compat files

--- compatibility/Coq__8_5beta2__Compat.v
+++ compatibility/Coq__8_5beta2__Compat.v
@@ -50,7 +50,7 @@ Require Coq.Numbers.Natural.Peano.NPeano.
 
 (** https://coq.inria.fr/bugs/show_bug.cgi?id=4461 *)
 Require Coq.Classes.RelationClasses.
-Global Arguments Antisymmetric A {eqA} _ _.
-Global Arguments symmetry {A} {R} {_} [x] [y] _.
-Global Arguments asymmetry {A} {R} {_} [x] [y] _ _.
-Global Arguments transitivity {A} {R} {_} [x] [y] [z] _ _.
+Global Arguments Coq.Classes.RelationClasses.Antisymmetric A {eqA} _ _.
+Global Arguments Coq.Classes.RelationClasses.symmetry {A} {R} {_} [x] [y] _.
+Global Arguments Coq.Classes.RelationClasses.asymmetry {A} {R} {_} [x] [y] _ _.
+Global Arguments Coq.Classes.RelationClasses.transitivity {A} {R} {_} [x] [y] [z] _ _.
