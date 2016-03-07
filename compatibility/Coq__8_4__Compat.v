3d1d28697e158fb39cbc582fb856e4c9bc234525
Jason Gross <jgross@mit.edu>
2016-03-07 17:29:27 -0500

    Add fast_set

--- compatibility/Coq__8_4__Compat.v
+++ compatibility/Coq__8_4__Compat.v
@@ -1,2 +1,9 @@
 Ltac shelve_unifiable := idtac.
 Ltac shelve := idtac.
+
+(** [set (x := y)] is about 50x slower than it needs to be in Coq 8.4,
+    but is about 4x faster than the alternatives in 8.5.  See
+    https://coq.inria.fr/bugs/show_bug.cgi?id=3280 (comment 13) for
+    more details. *)
+Ltac fast_set' x y := pose y as x; change y with x.
+Tactic Notation "fast_set" "(" ident(x) ":=" constr(y) ")" := fast_set' x y.
--- compatibility/Coq__8_5beta2__Compat.v
+++ compatibility/Coq__8_5beta2__Compat.v
@@ -54,3 +54,10 @@ Global Arguments Coq.Classes.RelationClasses.Antisymmetric A eqA {_} _.
 Global Arguments Coq.Classes.RelationClasses.symmetry {A} {R} {_} [x] [y] _.
 Global Arguments Coq.Classes.RelationClasses.asymmetry {A} {R} {_} [x] [y] _ _.
 Global Arguments Coq.Classes.RelationClasses.transitivity {A} {R} {_} [x] [y] [z] _ _.
+
+(** [set (x := y)] is about 50x slower than it needs to be in Coq 8.4,
+    but is about 4x faster than the alternatives in 8.5.  See
+    https://coq.inria.fr/bugs/show_bug.cgi?id=3280 (comment 13) for
+    more details. *)
+Ltac fast_set' x y := set (x := y).
+Tactic Notation "fast_set" "(" ident(x) ":=" constr(y) ")" := fast_set' x y.
