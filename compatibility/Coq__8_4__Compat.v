dadafb8b35bf1a25788b6d1bdc1b4d3a867c5a9b
Jason Gross <jgross@mit.edu>
2016-03-07 22:41:49 -0500

    Version of [set] that works with evars

--- compatibility/Coq__8_4__Compat.v
+++ compatibility/Coq__8_4__Compat.v
@@ -5,5 +5,13 @@ Ltac shelve := idtac.
     but is about 4x faster than the alternatives in 8.5.  See
     https://coq.inria.fr/bugs/show_bug.cgi?id=3280 (comment 13) for
     more details. *)
-Ltac fast_set' x y := pose y as x; change y with x.
+Ltac fast_set' x y :=
+  pose y as x;
+  first [ progress change y with x
+        | progress repeat match goal with
+                          | [ |- appcontext G[?e] ]
+                            => constr_eq y e;
+                               let G' := context G[x] in
+                               change G'
+                          end ].
 Tactic Notation "fast_set" "(" ident(x) ":=" constr(y) ")" := fast_set' x y.
