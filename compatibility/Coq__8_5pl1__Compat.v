23fe183c378bb3499ca88893d64429f49c54edcc
Jason Gross <jgross@mit.edu>
2016-05-13 17:17:24 -0400

    Remove code no longer needed for compatibility

--- compatibility/Coq__8_5pl1__Compat.v
+++ compatibility/Coq__8_5pl1__Compat.v
@@ -1,12 +1,5 @@
 (** See https://coq.inria.fr/bugs/show_bug.cgi?id=4319 for updates *)
 
-(** https://coq.inria.fr/bugs/show_bug.cgi?id=4461 *)
-Require Coq.Classes.RelationClasses.
-Global Arguments Coq.Classes.RelationClasses.Antisymmetric A eqA {_} _.
-Global Arguments Coq.Classes.RelationClasses.symmetry {A} {R} {_} [x] [y] _.
-Global Arguments Coq.Classes.RelationClasses.asymmetry {A} {R} {_} [x] [y] _ _.
-Global Arguments Coq.Classes.RelationClasses.transitivity {A} {R} {_} [x] [y] [z] _ _.
-
 (** [set (x := y)] is about 50x slower than it needs to be in Coq 8.4,
     but is about 4x faster than the alternatives in 8.5.  See
     https://coq.inria.fr/bugs/show_bug.cgi?id=3280 (comment 13) for
