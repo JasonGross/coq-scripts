22844c59fbec351e6cbf8a645c810b2413f9f16a
Jason Gross <jgross@mit.edu>
2016-06-14 16:13:41 -0400

    Fix another typo

--- compatibility/Coq__trunk__Compat.v
+++ compatibility/Coq__trunk__Compat.v
@@ -17,7 +17,7 @@ Notation " [ x ; .. ; y ] " := (cons x .. (cons y nil) ..) : list_scope.
 Notation " [ x ; y ; .. ; z ] " := (cons x (cons y .. (cons z nil) ..)) : list_scope.
 End LocalListNotations.
 Module Export LocalVectorNotations.
-Notation " [ ] " := (Vector.nil _) (format "[ ]") : vector_scope.
+Notation " [ ] " := (VectorDef.nil _) (format "[ ]") : vector_scope.
 Notation " [ x ; .. ; y ] " := (VectorDef.cons _ x _ .. (VectorDef.cons _ y _ (VectorDef.nil _)) ..) : vector_scope.
 Notation " [ x ; y ; .. ; z ] " := (VectorDef.cons _ x _ (VectorDef.cons _ y _ .. (VectorDef.cons _ z _ (VectorDef.nil _)) ..)) : vector_scope.
 End LocalVectorNotations.
