ac836d16587cf2836f2faa603cad463a7c7814b1
Jason Gross <jgross@mit.edu>
2016-07-04 17:36:49 -0400

    Add a bit of 8.6 compat

---
+++ compatibility/Coq__8_6__Compat.v
@@ -0,0 +1,41 @@
+(** See https://coq.inria.fr/bugs/show_bug.cgi?id=4319 for updates *)
+
+(** [set (x := y)] is about 50x slower than it needs to be in Coq 8.4,
+    but is about 4x faster than the alternatives in 8.5.  See
+    https://coq.inria.fr/bugs/show_bug.cgi?id=3280 (comment 13) for
+    more details. *)
+Ltac fast_set' x y := set (x := y).
+Ltac fast_set'_in x y H := set (x := y) in H.
+Tactic Notation "fast_set" "(" ident(x) ":=" constr(y) ")" := fast_set' x y.
+Tactic Notation "fast_set" "(" ident(x) ":=" constr(y) ")" "in" hyp(H) := fast_set'_in x y H.
+
+Require Coq.Lists.List.
+Require Coq.Vectors.VectorDef.
+Module Export LocalListNotations.
+Notation " [ ] " := nil (format "[ ]") : list_scope.
+Notation " [ x ; .. ; y ] " := (cons x .. (cons y nil) ..) : list_scope.
+Notation " [ x ; y ; .. ; z ] " := (cons x (cons y .. (cons z nil) ..)) : list_scope.
+End LocalListNotations.
+Module Export LocalVectorNotations.
+Notation " [ ] " := (VectorDef.nil _) (format "[ ]") : vector_scope.
+Notation " [ x ; .. ; y ] " := (VectorDef.cons _ x _ .. (VectorDef.cons _ y _ (VectorDef.nil _)) ..) : vector_scope.
+Notation " [ x ; y ; .. ; z ] " := (VectorDef.cons _ x _ (VectorDef.cons _ y _ .. (VectorDef.cons _ z _ (VectorDef.nil _)) ..)) : vector_scope.
+End LocalVectorNotations.
+Module Export Coq.
+Module Export Lists.
+Module List.
+Module ListNotations.
+Export Coq.Lists.List.ListNotations.
+Export LocalListNotations.
+End ListNotations.
+End List.
+End Lists.
+Module Export Vectors.
+Module VectorDef.
+Module VectorNotations.
+Export Coq.Vectors.VectorDef.VectorNotations.
+Export LocalVectorNotations.
+End VectorNotations.
+End VectorDef.
+End Vectors.
+End Coq.
