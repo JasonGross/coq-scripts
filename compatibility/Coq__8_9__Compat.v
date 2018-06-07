7aa4366478655517542ddc3b4bc3cd68be9ba081
Jason Gross <jgross@mit.edu>
2018-06-07 19:49:49 -0400

    Add support for 8.9

---
+++ compatibility/Coq__8_9__Compat.v
@@ -0,0 +1,25 @@
+(** See https://coq.inria.fr/bugs/show_bug.cgi?id=4319 for updates *)
+
+(** [set (x := y)] is about 50x slower than it needs to be in Coq 8.4,
+    but is about 4x faster than the alternatives in 8.5.  See
+    https://coq.inria.fr/bugs/show_bug.cgi?id=3280 (comment 13) for
+    more details. *)
+Ltac fast_set' x y := set (x := y).
+Ltac fast_set'_in x y H := set (x := y) in H.
+Tactic Notation "fast_set" "(" ident(x) ":=" constr(y) ")" := fast_set' x y.
+Tactic Notation "fast_set" "(" ident(x) ":=" constr(y) ")" "in" hyp(H) := fast_set'_in x y H.
+
+(** Add Coq 8.4+8.5 notations, so that we don't accidentally make use of Coq 8.4-only notations *)
+Require Coq.Lists.List.
+Require Coq.Vectors.VectorDef.
+Module Export LocalListNotations.
+Notation " [ ] " := nil (format "[ ]") : list_scope.
+Notation " [ x ; .. ; y ] " := (cons x .. (cons y nil) ..) : list_scope.
+Notation " [ x ; y ; .. ; z ] " := (cons x (cons y .. (cons z nil) ..)) : list_scope.
+End LocalListNotations.
+Module Export LocalVectorNotations.
+Notation " [ ] " := (VectorDef.nil _) (format "[ ]") : vector_scope.
+Notation " [ x ; .. ; y ] " := (VectorDef.cons _ x _ .. (VectorDef.cons _ y _ (VectorDef.nil _)) ..) : vector_scope.
+Notation " [ x ; y ; .. ; z ] " := (VectorDef.cons _ x _ (VectorDef.cons _ y _ .. (VectorDef.cons _ z _ (VectorDef.nil _)) ..)) : vector_scope.
+End LocalVectorNotations.
+Require Export Coq.funind.FunInd.
--- compatibility/generate-compat-files.sh
+++ compatibility/generate-compat-files.sh
@@ -17,17 +17,17 @@ function append_text() {
     done
 }
 
-ALL_VERSIONS="8_4 8_5beta1 8_5beta2 8_5beta3 8_5rc1 8_5 8_5pl1 8_6beta1 8_6 8_7 8_8 trunk master"
-VECTOR_LIST_VERSIONS="8_4 8_5beta1 8_5beta2 8_5beta3 8_5rc1 8_5 8_5pl1 8_6beta1 8_6 8_7 8_8 trunk master"
+ALL_VERSIONS="8_4 8_5beta1 8_5beta2 8_5beta3 8_5rc1 8_5 8_5pl1 8_6beta1 8_6 8_7 8_8 8_9 trunk master"
+VECTOR_LIST_VERSIONS="8_4 8_5beta1 8_5beta2 8_5beta3 8_5rc1 8_5 8_5pl1 8_6beta1 8_6 8_7 8_8 8_9 trunk master"
 NPEANO_VERSIONS="8_5beta1 8_5beta2 8_5beta3 8_5rc1 8_5 8_5pl1"
 FMAPFACTS_VERSIONS="8_5beta1 8_5beta2 8_5beta3 8_5rc1 8_5 8_5pl1"
 COQ_MODULE_VERSIONS="8_5beta1 8_5beta2 8_5beta3 8_5rc1 8_5 8_5pl1"
-FAST_SET_AS_SET_VERSIONS="8_5beta1 8_5beta2 8_5beta3 8_5rc1 8_5 8_5pl1 8_6beta1 8_6 8_7 8_8 trunk master"
+FAST_SET_AS_SET_VERSIONS="8_5beta1 8_5beta2 8_5beta3 8_5rc1 8_5 8_5pl1 8_6beta1 8_6 8_7 8_8 8_9 trunk master"
 RELATION_ARGUMENTS_VERSIONS="8_5beta1 8_5beta2 8_5beta3 8_5rc1 8_5"
 RAPPLY_SHELVE_VERSIONS="8_5beta1 8_5beta2 8_5beta3"
 MISC_BETA1_VERSIONS="8_5beta1 8_5beta2"
 INT_VERSIONS="8_5beta2"
-FUNIND_VERSIONS="8_7 8_8 trunk master"
+FUNIND_VERSIONS="8_7 8_8 8_9 trunk master"
 
 for v in $ALL_VERSIONS; do
     cp -f "Coq__${v}__Compat.v.in" "Coq__${v}__Compat.v"
