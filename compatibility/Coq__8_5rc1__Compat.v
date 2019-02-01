a23081e0f678054bedfe7905823240d46311a6d6
Clément Pit-Claudel <clement.pitclaudel@live.com>
2019-02-01 09:11:39 -0500

    Change VectorDef to Vector to work around OCaml extraction issues
    Closes #4.

--- compatibility/Coq__8_5rc1__Compat.v
+++ compatibility/Coq__8_5rc1__Compat.v
@@ -18,16 +18,16 @@ Global Arguments Coq.Classes.RelationClasses.transitivity {A} {R} {_} [x] [y] [z
 
 (** Add Coq 8.4+8.5 notations, so that we don't accidentally make use of Coq 8.4-only notations *)
 Require Coq.Lists.List.
-Require Coq.Vectors.VectorDef.
+Require Coq.Vectors.Vector.
 Module Export LocalListNotations.
 Notation " [ ] " := nil (format "[ ]") : list_scope.
 Notation " [ x ; .. ; y ] " := (cons x .. (cons y nil) ..) : list_scope.
 Notation " [ x ; y ; .. ; z ] " := (cons x (cons y .. (cons z nil) ..)) : list_scope.
 End LocalListNotations.
 Module Export LocalVectorNotations.
-Notation " [ ] " := (VectorDef.nil _) (format "[ ]") : vector_scope.
-Notation " [ x ; .. ; y ] " := (VectorDef.cons _ x _ .. (VectorDef.cons _ y _ (VectorDef.nil _)) ..) : vector_scope.
-Notation " [ x ; y ; .. ; z ] " := (VectorDef.cons _ x _ (VectorDef.cons _ y _ .. (VectorDef.cons _ z _ (VectorDef.nil _)) ..)) : vector_scope.
+Notation " [ ] " := (Vector.nil _) (format "[ ]") : vector_scope.
+Notation " [ x ; .. ; y ] " := (Vector.cons _ x _ .. (Vector.cons _ y _ (Vector.nil _)) ..) : vector_scope.
+Notation " [ x ; y ; .. ; z ] " := (Vector.cons _ x _ (Vector.cons _ y _ .. (Vector.cons _ z _ (Vector.nil _)) ..)) : vector_scope.
 End LocalVectorNotations.
 
 Require Coq.Arith.Even.
