6bcc15b6d018560ebc368efcfc2c2c7d435c7dcc
Jason Gross <jgross@mit.edu>
2019-08-22 03:00:38 -0400

    Switch from python2 to python3
    Closes #6

--- strictify-coqdep.py
+++ strictify-coqdep.py
@@ -1,10 +1,11 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python3
 import sys, subprocess
 import re
 
 if __name__ == '__main__':
     p = subprocess.Popen(sys.argv[1:], stderr=subprocess.PIPE)
     (stdout, stderr) = p.communicate()
+    stderr = stderr.decode('utf-8')
     reg = re.compile(r'''Warning(: in file .*?,\s*required library .*? matches several files in path)''')
     if reg.search(stderr):
         sys.stderr.write(reg.sub(r'Error\1', stderr))
