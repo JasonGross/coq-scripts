d306f551ad3806611f16a21577a27606c42dcc42
Jason Gross <jgross@mit.edu>
2019-11-02 18:29:21 -0400

    Update compat stuff to work with coq/coq#11032

--- compatibility/Coq__8_11__Compat.v
+++ compatibility/Coq__8_11__Compat.v
@@ -1,6 +1,25 @@
 (** See https://coq.inria.fr/bugs/show_bug.cgi?id=4319 for updates *)
 
-Require Export Coq.Compat.Coq88.
+(** Compatibility file for making Coq act similar to Coq v8.8 *)
+Local Set Warnings "-deprecated".
+
+Require Export Coq.Compat.Coq89.
+
+(** In Coq 8.9, prim token notations follow [Import] rather than
+    [Require].  So we make all of the relevant notations accessible in
+    compatibility mode. *)
+Require Coq.Strings.Ascii Coq.Strings.String.
+Export String.StringSyntax Ascii.AsciiSyntax.
+Require Coq.ZArith.BinIntDef Coq.PArith.BinPosDef Coq.NArith.BinNatDef.
+Require Coq.Reals.Rdefinitions.
+Require Coq.Numbers.Cyclic.Int63.Int63.
+Require Coq.Numbers.Cyclic.Int31.Int31.
+Declare ML Module "r_syntax_plugin".
+Declare ML Module "int63_syntax_plugin".
+Numeral Notation BinNums.Z BinIntDef.Z.of_int BinIntDef.Z.to_int : Z_scope.
+Numeral Notation BinNums.positive BinPosDef.Pos.of_int BinPosDef.Pos.to_int : positive_scope.
+Numeral Notation BinNums.N BinNatDef.N.of_int BinNatDef.N.to_int : N_scope.
+Numeral Notation Int31.int31 Int31.phi_inv_nonneg Int31.phi : int31_scope.
 
 (** [set (x := y)] is about 50x slower than it needs to be in Coq 8.4,
     but is about 4x faster than the alternatives in 8.5.  See
