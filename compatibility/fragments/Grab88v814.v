37c5b726706a0d28e91c79740afad9c36e705dbe
Jason Gross <jgross@mit.edu>
2021-02-23 10:57:39 -0500

    Update to num_int for compat with coq/coq#13842

--- compatibility/fragments/Grab88v814.v
+++ compatibility/fragments/Grab88v814.v
@@ -17,7 +17,7 @@ Require Coq.ZArith.BinIntDef Coq.PArith.BinPosDef Coq.NArith.BinNatDef.
 Require Coq.Reals.Rdefinitions.
 Require Coq.Numbers.Cyclic.Int63.Int63.
 Require Coq.Numbers.Cyclic.Int31.Int31.
-Numeral Notation BinNums.Z BinIntDef.Z.of_int BinIntDef.Z.to_int : Z_scope.
-Numeral Notation BinNums.positive BinPosDef.Pos.of_int BinPosDef.Pos.to_int : positive_scope.
-Numeral Notation BinNums.N BinNatDef.N.of_int BinNatDef.N.to_int : N_scope.
+Numeral Notation BinNums.Z BinIntDef.Z.of_num_int BinIntDef.Z.to_num_int : Z_scope.
+Numeral Notation BinNums.positive BinPosDef.Pos.of_num_int BinPosDef.Pos.to_num_int : positive_scope.
+Numeral Notation BinNums.N BinNatDef.N.of_num_int BinNatDef.N.to_num_int : N_scope.
 Numeral Notation Int31.int31 Int31.phi_inv_nonneg Int31.phi : int31_scope.
