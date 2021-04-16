e443917a6b829ac56c853646add7d9bffc97ad49
Jason Gross <jgross@mit.edu>
2021-04-16 16:10:26 -0400

    Fancier handling of warnings and errors

--- github/reportify-coq.sh
+++ github/reportify-coq.sh
@@ -3,4 +3,38 @@
 set -e
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
-"$DIR/reportify-coq-errors-gen.sh" 'Warning\|Error' "$@"
+
+do_error=""
+do_warning=""
+
+for i in 1 2; do
+    if [ "$1" == "--warnings" ]; then
+        do_warning=yes
+        shift
+    elif [ "$1" == "--errors" ]; then
+        do_error=yes
+        shift
+    fi
+done
+
+pat=""
+
+case $do_warning,$do_error in
+    yes,yes)
+        pat='Warning\|Error'
+        ;;
+    yes,)
+        pat='Warning'
+        ;;
+    ,yes)
+        pat='Error'
+        ;;
+    *)
+        ;;
+esac
+
+if [ -z "$pat" ]; then
+    "$@"
+else
+    "$@" 2> >("$DIR/reportify-coq-errors-gen.sh" "$pat")
+fi
