9e57db9a9ad3bba8f57fa69b8ace99653f99235d
Jason Gross <jgross@mit.edu>
2021-04-22 15:51:53 -0400

    Add a fancier reportify script
    This script can now handle the full output of `make` and will do its
    best to detect when errors have terminated.  We also behave better with
    regard to ensuring that we emit output once the parent process has
    finished.

--- github/reportify-coq-errors-gen.sh
+++ github/reportify-coq-errors-gen.sh
@@ -4,34 +4,84 @@ set -e
 
 # e.g., "Warning\|Error"
 warnerr="$1"
-curerr=""
+curlines=()
+first_line_regex='\(-\s\+\)\?File "\([^ "]\+\)", line \([0-9]\+\), characters \([0-9]\+-[0-9]\+\):\s*'
+second_line_regex='\(-\s\+\)\?\('"$warnerr"'\):\s*'
+last_line_regex='\[\([^],]\+\),\([^],]\+\)\]\s*$'
+file_line_char_warn_regex="^${first_line_regex}"'\(%0A\s*\)\?'"${second_line_regex}"
+invalid_error_regex=(
+    "^${first_line_regex}"
+    '^\(-\s\+\)\?\(COQC\|OCAMLC\|OCAMLOPT\|COQDEP\) ' # new build file, presumably we missed the end of the error
+    '^\(-\s\+\)\?[^\s]\+ (real: [0-9\.]*, user: [0-9\.]*, sys: [0-9\.]*, mem: [0-9]* ko)' # output of make TIMED=1, we probably missed the end of the warning/error
+    '^\(-\s\+\)\?make\(\[[0-9]\+\]\):' # we ended up back in make output
+    '^\(-\s\+\)\?::' # already a message to GH
+)
 
-file_line_char_warn_regex='^File "\([^ "]\+\)", line \([0-9]\+\), characters \([0-9]\+-[0-9]\+\):\s*\(%0A\s*\)\?\('"$warnerr"'\):\s*'
+if [[ ! -v skip_unterminated_message_warning ]]; then
+    # default value
+    skip_unterminated_message_warning="" # empty means "don't skip"
+fi
 
-function process() {
+function format_message() {
     if echo "$1" | grep -q "${file_line_char_warn_regex}"; then
-        echo "$1" | sed "s~${file_line_char_warn_regex}~"'::error severity=\5,file=\1,line=\2,col=\3::~g; s~^\(::[^:]*\)::\(.*\)\[\([^,]\+\),\([^]]\+\)\]\s*$~\1,code=\3%2C\4::\2~g; s/^::error severity=[Ee][Rr][Rr][Oo][Rr],/::error /g; s/^::error severity=[Ww][Aa][Rr][Nn][Ii][Nn][Gg],/::warning /g'
+        echo "$1" | sed "s~${file_line_char_warn_regex}~"'::error severity=\7,file=\2,line=\3,col=\4::~g; s~^\(::[^:]*\)::\(.*\)\[\([^,]\+\),\([^]]\+\)\]\s*$~\1,code=\3%2C\4::\2~g; s/^::error severity=[Ee][Rr][Rr][Oo][Rr],/::error /g; s/^::error severity=[Ww][Aa][Rr][Nn][Ii][Nn][Gg],/::warning /g'
     else
         echo "$1" | sed 's/%0A/\n/g'
     fi
 }
 
-while read i
-do
-    # ^File \"([^ \"]+)\", line (\\d+), characters (\\d+-\\d+):
-    if [[ "$i" == "File "*:* ]]; then # first line of error
-        if [ ! -z "$curerr" ]; then
-            process "$curerr"
+function join_to_oneline() {
+    sep=""
+    for i in "$@"; do
+        echo -n "${sep}${i}"
+        sep="%0A"
+    done
+}
+
+function format_curlines_first_two() {
+    if [ -z "${skip_unterminated_message_warning}" ]; then # first we report on the fact that we have an unterminated warning:
+        echo -n "::warning::Could not find a terminator for warning:%0A"
+        join_to_oneline "${curlines[@]}"
+        echo
+    fi
+    format_message "$(join_to_oneline "${curlines[@]:0:2}")" # slice of length 2 starting at 0
+    for i in "${curlines[@]:2}"; do # slice to the end starting from 2
+        echo "$i"
+    done
+}
+
+function invalid_line() {
+    for i in "${invalid_error_regex[@]}"; do
+        if echo "$1" | grep -q "$i"; then
+            return 0 # we found an invalid line, so we succeed
         fi
-        curerr="$i"
-    elif [[ "$i" == "::"* ]]; then # already a message to GH
-        echo "$i"
-    elif [ ! -z "$curerr" ]; then
-        curerr+="%0A$i"
-    else # not part of a recognized error
-        echo "$i"
+    done
+    return 1
+}
+
+while read line
+do
+    if [ "${#curlines[@]}" -gt 0 ]; then # we're in the middle of an error
+        if echo "$line" | grep -q "${last_line_regex}"; then # we found the end
+            curlines+=("$line")
+            format_message "$(join_to_oneline "${curlines[@]}")"
+            curlines=()
+            continue
+        elif invalid_line "$line"; then # this line is not a valid part of an error, so we print only the first two lines and then dump the rest
+            format_curlines_first_two
+            curlines=()
+        fi
+    fi
+    if [ "${#curlines[@]}" -gt 0 ]; then # we're still in the middle of an error, so just accumulate
+        curlines+=("$line")
+    elif echo "$line" | grep -q "^${first_line_regex}"; then # we weren't in an error, and we've now found the first line of an error
+        curlines+=("$line")
+    else # no error here, just pipe through
+        echo "$line"
     fi
 done
-if [ ! -z "$curerr" ]; then
-    process "$curerr"
+
+if [ "${#curlines[@]}" -gt 0 ]; then # we have an unterminated error, just print the first two lines
+    format_curlines_first_two
+    curlines=()
 fi
--- github/reportify-coq.sh
+++ github/reportify-coq.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-set -e
+set -e -o pipefail
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
@@ -36,5 +36,6 @@ esac
 if [ -z "$pat" ]; then
     "$@"
 else
-    "$@" 2> >("$DIR/reportify-coq-errors-gen.sh" "$pat" >&2)
+    # we might just do 2> >(script >&2), but the script might die, cf https://stackoverflow.com/questions/3618078/pipe-only-stderr-through-a-filter/52575213#comment105555334_52575087
+    { "$@" 3>&1 1>&2 2>&3 3>&- | "$DIR/reportify-coq-errors-gen.sh" "$pat"; } 3>&1 1>&2 2>&3 3>&-
 fi
