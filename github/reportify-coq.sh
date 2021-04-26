1ad4075c553415ba01f27796231ef26019e76093
Jason Gross <jgross@mit.edu>
2021-04-26 07:20:22 -0400

    Add --stdout to support reading from stdout for GH Actions

--- github/reportify-coq.sh
+++ github/reportify-coq.sh
@@ -6,13 +6,17 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
 do_error=""
 do_warning=""
+stdout=""
 
-for i in 1 2; do
+for i in 1 2 3; do
     if [ "$1" == "--warnings" ]; then
         do_warning=yes
         shift
     elif [ "$1" == "--errors" ]; then
         do_error=yes
+        shift
+    elif [ "$1" == "--stdout" ]; then
+        stdout=yes
         shift
     fi
 done
@@ -35,7 +39,9 @@ esac
 
 if [ -z "$pat" ]; then
     "$@"
-else
+elif [ -z "$stdout" ]; then
     # we might just do 2> >(script >&2), but the script might die, cf https://stackoverflow.com/questions/3618078/pipe-only-stderr-through-a-filter/52575213#comment105555334_52575087
     { "$@" 3>&1 1>&2 2>&3 3>&- | "$DIR/reportify-coq-errors-gen.sh" "$pat"; } 3>&1 1>&2 2>&3 3>&-
+else
+    "$@" | "$DIR/reportify-coq-errors-gen.sh" "$pat"
 fi
